162ca8d245d9f4d0031d3b48bfe33ecd68b6c12e
TDT4258 Student <tdt4258@tdt4258-VirtualBox.(none)>
2017-11-03 11:58:08 +0100

    Game minior update

--- local_src/game-1.0/game.c
+++ local_src/game-1.0/game.c
@@ -2,9 +2,18 @@
 #include <stdlib.h>
 #include "display.h"
 
+int initGamepad();
+
 int main(int argc, char *argv[])
 {
 	printf("Hello World, I'm game!\n");
 	initDisplay();
 	exit(EXIT_SUCCESS);
+}
+
+int initGamepad(){
+	gamepad = fopen("/dev/gamepad", "rb");
+	if(!gamepad){
+		return EXIT_FAILURE;
+	}
 }
